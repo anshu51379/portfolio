c0bb2438404bf5375944192579bbf9eb3a2124af
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-06-25 17:32:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/app.scss";
+import ColorModeSwitcher from "./ColorModeSwitcher";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ColorModeSwitcher />
     <App />
   </React.StrictMode>
 );
